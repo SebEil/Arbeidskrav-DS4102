154e8c604fa228bd873dc23a2fa7c673f415e31a
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2022-02-15 14:27:04 +0100

    test

--- src/components/views/aboutPage.vue
+++ src/components/views/aboutPage.vue
@@ -1,8 +1,10 @@
 <template>
     <h3>About us</h3>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
         Provident illum beatae hic quaerat natus deserunt! Consectetur,
         minima ullam ad delectus quam itaque voluptatum veniam, inventore,
         debitis facilis corporis hic a? Id adipisci officiis saepe! Atque facere
-        aspernatur cupiditate nihil quo!</p>
+        aspernatur cupiditate nihil quo!
+    </p>
 </template>
