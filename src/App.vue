c4d794f0bba349e90386921f6070ad632980ca14
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2022-02-15 14:57:08 +0100

    fikset routing for begge sider

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
 <main>
   <nav-bar></nav-bar>
-  <exercise-header></exercise-header>
-  <br>
-  <br>
-  <br>
-  <exercise-list></exercise-list>
-  <add-exercise></add-exercise>
 </main>  
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import exerciseList from "./components/exercises/exerciseList.vue"
-import exerciseHeader from "./components/exerciseHeader.vue"
-import addExercise from "./components/exercises/addExercise.vue"
 import navBar from "./components/navBar.vue"
 
 export default {
   name: 'App',
   components: {
-    exerciseList,
-    exerciseHeader,
-    addExercise,
     navBar
   }
 }
---
+++ src/components/views/homePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <exercise-list></exercise-list>
+  <add-exercise></add-exercise>
+</template>
+
+<script>
+import exerciseList from "../exercises/exerciseList.vue"
+import addExercise from "../exercises/addExercise.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    exerciseList,
+    addExercise
+  }
+}
+</script>
