96648feb89f1518db74a16f0ca679db4c537ed41
SebEil <56046884+SebEil@users.noreply.github.com>
2022-02-15 19:41:10 +0100

    Skifter farge utifra ja eller nei, brukt v-bind

--- src/components/exercises/exerciseType.vue
+++ src/components/exercises/exerciseType.vue
@@ -3,7 +3,9 @@
         <h3>{{ øvelse }}</h3>
         <h4>Muskelgruppe som brukes: {{ targets }}</h4>
         <h4>For økter hvor du skal trene: {{ split }} </h4>
+        <div v-bind:class="[vekter === 'Ja' ? 'trenger' : 'trenger-ikke']">
         <h4>Trenger man utstyr: {{ vekter }}</h4>
+        </div>
         <img :src="getImage()"> 
     </section>             
 </template>
