8a643733508029a3cc62b8fc29c131238686973f
SebEil <56046884+SebEil@users.noreply.github.com>
2022-02-17 13:11:06 +0100

    La til flere øvelser

--- src/services/exerciseServise.js
+++ src/services/exerciseServise.js
@@ -6,7 +6,11 @@ const exerciseService = ( function(){
         { øvelse: "Benchpress", targets: "Chest", split: "Upperbody", vekter: "Ja", image: "benchpress.jpg"},
         { øvelse: "Pull-up", targets: "Back", split: "Upperbody", vekter: "Ja", image: "pullup.jpg"},
         { øvelse: "Shoulderpress", targets: "Shoulders", split: "Upperbody", vekter: "Ja", image: "shoulderpress.jpg"},
-        { øvelse: "Leg-press", targets: "Legs", split: "Lowerbody", vekter: "Ja", image: "legpress.jpg"}
+        { øvelse: "Leg-press", targets: "Legs", split: "Lowerbody", vekter: "Ja", image: "legpress.jpg"},
+        { øvelse: "Biceps-curls", targets: "Arms", split: "Upperbody", vekter: "Ja", image: "biceps-curls.jpg"},
+        { øvelse: "Pushups", targets: "Chest", split: "Upperbody", vekter: "Nei", image: "pushups.jpg"},
+        { øvelse: "Sit-ups", targets: "Abs", split: "Upperbody", vekter: "Nei", image: "sit-ups.jpg"},
+        { øvelse: "Squat", targets: "Legs", split: "Lowerbody", vekter: "Nei", image: "squat.jpg"}
     ]);
 
     const getAll = () => {
